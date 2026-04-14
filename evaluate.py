42685f43bb75cc23045f8173228e947f74b22be1
adikonn <kalmurzinadilet@gmail.com>
2026-04-14 06:52:04 +0300

    feat: add counting metric otdel'no na triang matrices

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+
 from train import load, inference
 
 
@@ -13,19 +14,27 @@ def main():
     model = load()
     data = np.load("dataset.npz")
     total_metric = 0
+    triang_metric = 0
+    triang_cnt = 0
     for i in range(len(data['A'])):
         A = torch.tensor(data['A'][i], dtype=torch.float32)
         B = torch.tensor(data['B'][i], dtype=torch.float32)
-
+        triang = data['y'][i]
         T = inference(model, A, B)
-
-        M_A = (T.T @ A @ T).detach().cpu().numpy()
-        M_B = (T.T @ B @ T).detach().cpu().numpy()
+        T_inv = torch.inverse(T)
+        M_A = (T_inv @ A @ T).detach().cpu().numpy()
+        M_B = (T_inv @ B @ T).detach().cpu().numpy()
 
         metric_A = calc_lower_diag_metric(M_A)
         metric_B = calc_lower_diag_metric(M_B)
         total_metric += (metric_A + metric_B)
+        if (triang == 1):
+            triang_cnt += 1
+            triang_metric += (metric_A + metric_B)
 
-    print(f"Your Score: {((1/len(data['A']))*total_metric):.4f}")
+    print(f"General Score: {((1 / len(data['A'])) * total_metric):.4f}")
+    print(f"Score for triang matrices: {((1 / triang_cnt) * triang_metric):.4f}")
+
+
 if __name__ == "__main__":
     main()
